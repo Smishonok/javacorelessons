19392dcfeb0fe2764452da48b35e07d09e108622
Valentin Nikolaev <valentinenikolaev@gmail.com>
2020-06-11 18:54:09 +0300

    Method "addRegion" deleted from AddRegionRequestHandler, instead this method added method "processRequest" and "getHelpForAddingRegionDataRequest."

--- src/main/java/com/valentin_nikolaev/javacore/finalWork/view/RegionRequestsHandlers/AddRegionRequestHandler.java
+++ src/main/java/com/valentin_nikolaev/javacore/finalWork/view/RegionRequestsHandlers/AddRegionRequestHandler.java
@@ -12,18 +12,31 @@ public class AddRegionRequestHandler extends RegionRequestHandler {
     public void handleRequest(String action, List<String> options) throws ClassNotFoundException {
         if (ADD.equals(action)) {
             this.regionController = new RegionController();
-            addRegion(options);
+            processRequest(options);
         } else {
-            getNextHandler(action,options);
+            getNextHandler(action, options);
         }
     }
 
-    private void addRegion(List<String> options) {
-        if (options.size() == 1) {
-            this.regionController.addRegion(options.get(0));
-        } else {
-            System.out.println("Invalid request format. Please, check request format and try " +
-                                       "again, or get help information.");
+    private void processRequest(List<String> options) {
+        if (options.size() != 1) {
+            System.out.println(
+                    "Invalid request format. Please, check request format and try again, or get " +
+                            "help information.");
+            return;
         }
+
+        if (options.get(0).equals(HELP)) {
+            getHelpForAddingRegionDataRequest();
+        } else {
+            this.regionController.addRegion(options.get(0));
+        }
+    }
+
+    private void getHelpForAddingRegionDataRequest() {
+        String helpInfo = "For adding regions into the repository it can be used next formats of" +
+                "request:\n" + "\t1: " + ADD + " [region name]";
+
+        System.out.println(helpInfo);
     }
 }
