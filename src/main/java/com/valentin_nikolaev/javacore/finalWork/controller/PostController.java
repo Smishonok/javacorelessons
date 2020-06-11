72f022547ae1eabb371c0ed92d5e5b650cf4f4b0
Valentin Nikolaev <valentinenikolaev@gmail.com>
2020-06-11 19:57:31 +0300

    Adding the check-in method "addPost" for checking exists of the user, which creates the post.

--- src/main/java/com/valentin_nikolaev/javacore/finalWork/controller/PostController.java
+++ src/main/java/com/valentin_nikolaev/javacore/finalWork/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.valentin_nikolaev.javacore.finalWork.controller;
 
 import com.valentin_nikolaev.javacore.finalWork.models.Post;
+import com.valentin_nikolaev.javacore.finalWork.models.User;
 import com.valentin_nikolaev.javacore.finalWork.repository.PostRepository;
 import com.valentin_nikolaev.javacore.finalWork.repository.RepositoryManager;
 import org.apache.log4j.Logger;
@@ -14,9 +15,11 @@ public class PostController {
     static Logger log = Logger.getLogger(PostController.class);
 
     private PostRepository postRepository;
+    private UserController userController;
 
     public PostController() throws ClassNotFoundException {
         initPostRepository();
+        this.userController = new UserController();
     }
 
     private void initPostRepository() throws ClassNotFoundException {
@@ -26,13 +29,18 @@ public class PostController {
     }
 
     public void addPost(String userId, String content) {
-        long id = Long.parseLong(userId);
-        Post post = new Post(id, content);
-        this.postRepository.add(post);
+        Optional<User> user = this.userController.getUserById(userId);
+        if (user.isPresent()) {
+            long id   = Long.parseLong(userId);
+            Post post = new Post(id, content);
+            this.postRepository.add(post);
+        } else {
+            throw new IllegalArgumentException("The user with id: " + userId + " is not exists.");
+        }
     }
 
     public Optional<Post> getPost(String postId) {
-        long id = Long.parseLong(postId);
+        long           id   = Long.parseLong(postId);
         Optional<Post> post = Optional.empty();
         if (this.postRepository.contains(id)) {
             post = Optional.of(this.postRepository.get(id));
@@ -49,7 +57,7 @@ public class PostController {
         return this.postRepository.getPostsByUserId(id);
     }
 
-    public void changePost(String postId,String newContent) {
+    public void changePost(String postId, String newContent) {
         long id = Long.parseLong(postId);
         if (this.postRepository.contains(id)) {
             Post post = this.postRepository.get(id);
